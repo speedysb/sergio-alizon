ec8f79e6fdfed3bd6f8c22edce3f41c0030a8737
speedysb <santijb534@gmail.com>
2026-05-08 15:05:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 
+function sendMessage(){
+
+  let message =
+  "Quiero compartir contigo este día tan especial, cuenta con nuestra presencia. 💍";
+
+  let phone = "13057201027";
+
+  let url =
+  `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
+  window.open(url, "_blank");
+
+}
+
+/* FOOD OPTIONS */
+
+function selectFood(food){
+
+  let message =
+  `Hola, quiero escoger ${food} como mi opción de menú 🍽️`;
+
+  let phone = "13057201027";
+
+  let url =
+  `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
+  window.open(url, "_blank");
+
+}
