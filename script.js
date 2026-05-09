830cffdf43e44eed0ffa2f63cb213288b607a768
speedysb <santijb534@gmail.com>
2026-05-08 21:28:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* STORY SLIDER */
+
 const storySlides =
 document.querySelectorAll(".story-slide");
 
@@ -18,3 +20,40 @@ setInterval(() => {
   .classList.add("active");
 
 }, 7000);
+
+/* FAVORITE SLIDER */
+
+const favoriteSlides =
+document.querySelectorAll(".favorite-slide");
+
+let currentFavorite = 0;
+
+setInterval(() => {
+
+  favoriteSlides[currentFavorite]
+  .classList.remove("active-favorite");
+
+  currentFavorite++;
+
+  if(currentFavorite >= favoriteSlides.length){
+    currentFavorite = 0;
+  }
+
+  favoriteSlides[currentFavorite]
+  .classList.add("active-favorite");
+
+}, 2500);
+
+/* MOBILE MENU */
+
+const menuToggle =
+document.getElementById("menu-toggle");
+
+const navLinks =
+document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+  navLinks.classList.toggle("show-menu");
+
+});
