a0dc98d1f1f2f164214ed1f85eb53093e22c2821
speedysb <santijb534@gmail.com>
2026-05-08 21:22:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-/* STORY SLIDESHOW */
-
-const storySlides = document.querySelectorAll(".story-slide");
+const storySlides =
+document.querySelectorAll(".story-slide");
 
 let currentStory = 0;
 
 setInterval(() => {
 
-  storySlides[currentStory].classList.remove("active");
+  storySlides[currentStory]
+  .classList.remove("active");
 
   currentStory++;
 
@@ -14,31 +14,7 @@ setInterval(() => {
     currentStory = 0;
   }
 
-  storySlides[currentStory].classList.add("active");
+  storySlides[currentStory]
+  .classList.add("active");
 
 }, 7000);
-
-/* FAVORITE PHOTOS AUTO ANIMATION */
-
-const favoritePhotos =
-document.querySelectorAll(".favorite-gallery img");
-
-let currentPhoto = 0;
-
-setInterval(() => {
-
-  favoritePhotos.forEach(photo => {
-    photo.style.opacity = "0.4";
-    photo.style.transform = "scale(0.96)";
-  });
-
-  favoritePhotos[currentPhoto].style.opacity = "1";
-  favoritePhotos[currentPhoto].style.transform = "scale(1.05)";
-
-  currentPhoto++;
-
-  if(currentPhoto >= favoritePhotos.length){
-    currentPhoto = 0;
-  }
-
-}, 2500);
